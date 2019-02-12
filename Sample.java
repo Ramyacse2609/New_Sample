2887d31b9ddd613af04b87f07df4c0b7e4be792f
Ramyacse2609 <ramyasriv1995@outlook.com>
2019-02-12 12:10:51 +0530

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -1,7 +1,6 @@
-Class Sample
-{
-public static void main(String args[])
-{
-	System.out.println("Hello World");
-}
-}
+class Simple 
+{ 
+	public static void main(String args[])
+	{ 
+		System.out.println("Hello Java"); 
+	} }
